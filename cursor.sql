6decc971652e920b0a3b432106deb6102da929c4
sharmadotexe <rockvikas300@gmail.com>
2024-07-05 11:05:43 +0530

    Cursor implemented in PL/SQL

---
+++ cursor.sql
@@ -0,0 +1,30 @@
+Declare
+    Cursor emp_cursor is
+      select employee_id, first_name, last_name
+      from employees
+      where salary > 5000;
+
+
+
+ v_empid employees.employee_id%type;
+ v_firstn employees.first_name%type;
+ v_elastn employees.last_name%type;
+
+
+Begin 
+  open emp_cursor;
+    Loop
+    Fetch emp_cursor into v_empid, v_firstn, v_elastn;
+    Exit when emp_cursor%NOTFOUND;
+
+
+
+  Dbms_output.put_line('Employee id is: ' ||v_empid );
+  Dbms_output.put_line('Employee first name is: ' || v_firstn );
+  Dbms_output.put_line('Employee last name is: ' || v_elastn) ;
+
+
+End loop;
+close emp_cursor;
+end;
+/
